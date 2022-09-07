4db9392760249a94d5058a7ac13c46a23b3d7656
priyanka866 <priyanka6396.pandey@gmail.com>
2022-09-07 23:21:06 +0530

    leetcode

--- leetcode_easy.sql
+++ leetcode_easy.sql
@@ -2,3 +2,11 @@
 
 select firstName,LastName,city,state from Person p
 left join Address a on p.personID=a.personID;
+
+##181. Employees Earning More Than Their Managers
+
+select e.name as Employee from Employee p 
+left join Employee e on p.id= e.managerID
+where e.salary> p.salary;
+
+
